d65d1bfb708c30ad7f2c8839a3b254bc261fed42
Hannes Heyden <hannesheyden@gmail.com>
2019-08-06 13:41:45 +0200

    make kioskview responsive

--- src/Components/PullRequestCard.tsx
+++ src/Components/PullRequestCard.tsx
@@ -6,12 +6,38 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { IconName } from '@fortawesome/fontawesome-svg-core';
 
 const PullRequestWrapper = styled.div`
+  position: relative;
   margin: 10px;
   box-sizing: border-box;
-  border-radius: 2px;
+  border-radius: 4px;
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
   background-color: ${props => props.theme.colors.white};
-  width: 30%;
+  min-width: 300px;
+  overflow: hidden;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+
+  @media screen and (max-width: 680px) {
+    width: 98%;
+    margin: 10px 0;
+  }
+
+  @media screen and (min-width: 680px) {
+    width: 46%;
+  }
+
+  @media screen and (min-width: 1000px) {
+    width: 31%;
+  }
+
+  @media screen and (min-width: 1540px) {
+    width: 32%;
+  }
+
+  @media screen and (min-width: 2040px) {
+    width: 24%;
+  }
 `;
 
 const Header = styled.div`
@@ -27,7 +53,6 @@ const Title = styled.h2`
   margin-left: 20px;
   font-size: 20px;
   line-height: 1.5;
-  flex-shrink: 1;
   font-weight: normal;
 `;
 
@@ -42,11 +67,20 @@ const Project = styled.div`
   display: flex;
   align-items: center;
   padding: 20px;
-  box-shadow: inset -17px 59px 39px -69px rgba(0, 0, 0, 0.25);
-  font-size: 20px;
+  border-top: 1px solid ${props => props.theme.colors.contrast};
+  font-size: 1rem;
 
   img {
     margin-right: 10px;
+  }
+
+  &:before {
+    content: 'Project:';
+    margin-right: 5px;
+    font-weight: bold;
+    text-transform: uppercase;
+    font-size: 12px;
+    color: #9b9b9b;
   }
 `;
 const Section = styled.div`
@@ -64,20 +98,49 @@ const ReviewCount = styled.div<{ count: number }>`
   margin-right: 20px;
   border-radius: 50%;
   color: ${props => props.theme.colors.white};
-  background-color: ${props => (props.count > 0 ? props.theme.colors.success : props.theme.colors.error)};
+  background-color: ${props =>
+    props.count > 0 ? props.theme.colors.success : props.theme.colors.error};
 `;
 
 const MergeButton = styled.a<{ mergeState: 'ready' | 'block' | 'warn' }>`
   padding: 20px;
   text-align: center;
-  display: block;
+  display: flex;
+  justify-content: center;
+  text-decoration: none;
+  align-items: center;
   color: ${props => props.theme.colors.white};
-  background-color: ${props => (props.mergeState === 'ready' ? props.theme.colors.success : props.mergeState === 'block' ? props.theme.colors.error : props.theme.colors.warning)};
+  background-color: ${props =>
+    props.mergeState === 'ready'
+      ? props.theme.colors.success
+      : props.mergeState === 'block'
+      ? props.theme.colors.error
+      : props.theme.colors.warning};
+
+  span {
+    font-weight: bold;
+    font-size: 14px;
+    text-transform: uppercase;
+    margin-left: 10px;
+  }
 `;
 
-const PullRequestCard: React.FC<latestPrs_search_nodes_PullRequest> = React.memo(props => {
-  const mergeState = props.mergeStateStatus === 'CLEAN' ? 'ready' : props.mergeStateStatus === 'BLOCKED' ? 'block' : 'warn';
-  const mergeIcon: IconName = mergeState === 'ready' ? 'check-double' : mergeState === 'block' ? 'stop-circle' : 'exclamation-circle';
+const PullRequestCard: React.FC<
+  latestPrs_search_nodes_PullRequest
+> = React.memo(props => {
+  const mergeState =
+    props.mergeStateStatus === 'CLEAN'
+      ? 'ready'
+      : props.mergeStateStatus === 'BLOCKED'
+      ? 'block'
+      : 'warn';
+
+  const mergeIcon: IconName =
+    mergeState === 'ready'
+      ? 'check-double'
+      : mergeState === 'block'
+      ? 'stop-circle'
+      : 'exclamation-circle';
 
   return (
     <PullRequestWrapper>
@@ -89,31 +152,74 @@ const PullRequestCard: React.FC<latestPrs_search_nodes_PullRequest> = React.memo
         </Title>
       </Header>
       <Project>
-        <Avatar size={25} src={props.repository.owner.avatarUrl} /> {props.repository.name}
+        <Avatar size={25} src={props.repository.owner.avatarUrl} />{' '}
+        {props.repository.name}
       </Project>
       <Section>
-        <FontAwesomeIcon style={{ marginRight: '10px' }} icon={['fas', 'code-branch']} />
-        {props.baseRefName} <FontAwesomeIcon style={{ margin: '0 10px' }} icon={['fas', 'arrow-circle-left']} /> {props.headRefName.substring(0, 30)}
+        <FontAwesomeIcon
+          style={{ marginRight: '10px' }}
+          icon={['fas', 'code-branch']}
+        />
+        {props.baseRefName}{' '}
+        <FontAwesomeIcon
+          style={{ margin: '0 10px' }}
+          icon={['fas', 'arrow-circle-left']}
+        />{' '}
+        {props.headRefName.substring(0, 30)}
         {props.headRefName.length > 30 && '...'}
       </Section>
       <Section>
-        <div style={{ width: '100%', display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+        <div
+          style={{
+            width: '100%',
+            display: 'flex',
+            justifyContent: 'space-between',
+            alignItems: 'center'
+          }}
+        >
           <div style={{ display: 'flex' }}>
             <ReviewCount count={1}>
               <FontAwesomeIcon icon={['fas', 'check-double']} size="1x" />
             </ReviewCount>
-            {props.reviews.nodes.map(n => (n.state === 'APPROVED' ? <Avatar key={n.author.avatarUrl} size={36} src={n.author.avatarUrl} /> : null))}
+            {props.reviews.nodes.map(n =>
+              n.state === 'APPROVED' ? (
+                <Avatar
+                  key={n.author.avatarUrl}
+                  size={36}
+                  src={n.author.avatarUrl}
+                />
+              ) : null
+            )}
           </div>
           <div style={{ display: 'flex' }}>
             <ReviewCount count={0}>
               <FontAwesomeIcon icon={['fas', 'check-double']} size="1x" />
             </ReviewCount>
-            {props.reviews.nodes.map(n => (n.state === 'CHANGES_REQUESTED' ? <Avatar key={n.author.avatarUrl} size={36} src={n.author.avatarUrl} /> : null))}
+            {props.reviews.nodes.map(n =>
+              n.state === 'CHANGES_REQUESTED' ? (
+                <Avatar
+                  key={n.author.avatarUrl}
+                  size={36}
+                  src={n.author.avatarUrl}
+                />
+              ) : null
+            )}
           </div>
         </div>
       </Section>
-      <MergeButton target="_blank" href={props.permalink} mergeState={mergeState}>
+      <MergeButton
+        target="_blank"
+        href={props.permalink}
+        mergeState={mergeState}
+      >
         <FontAwesomeIcon icon={['fas', mergeIcon]} size="2x" />
+        {mergeState === 'ready' ? (
+          <span>approved</span>
+        ) : mergeState === 'warn' ? (
+          <span>warning</span>
+        ) : (
+          <span>review</span>
+        )}
       </MergeButton>
     </PullRequestWrapper>
   );
